27d09e23ebf1051fb2fa8196f0f8cc9c0173145d
Andy Stoffel <acstoffel@gmail.com>
2025-08-11 18:14:16 -0400

    Task 8 static code analysis

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-import json
+"""flask server to run emotion detector"""
 from flask import Flask, request,render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def sent_emotion():  
+def sent_emotion():
+    """send string to NLP Watson EMotion Detection engine"""
     text_to_analyze = request.args.get('textToAnalyze')
     response_dict = emotion_detector(text_to_analyze)
     anger_score = response_dict.get('anger')
@@ -14,11 +15,11 @@ def sent_emotion():
     joy_score = response_dict.get('joy')
     sadness_score = response_dict.get('sadness')
     dominant_emotion = response_dict.get('dominant_emotion')
-    
+
     # return results
     if dominant_emotion == 'None':
-        return f"Invalid text! Please try again!."
-    
+        return "Invalid text! Please try again!."
+
     return f"For the given statement, the system response is 'anger': {anger_score},\
     'disgust': {disgust_score},\
     'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}.\
@@ -26,6 +27,7 @@ def sent_emotion():
 
 @app.route("/")
 def render_index_page():
+    """Load index template"""
     return render_template('index.html')
 
 if __name__ == "__main__":
