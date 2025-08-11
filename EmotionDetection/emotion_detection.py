4fd108c4aa9d339fecca070b23c3d85374abd78f
Andy Stoffel <acstoffel@gmail.com>
2025-08-11 18:02:15 -0400

    Task 7 Error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,23 +2,27 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    payload = {"raw_document": { "text": text_to_analyse } }
-    
+    payload = {"raw_document": { "text": text_to_analyse } }   
     response = requests.post(url, json = payload, headers = headers)
-    response_dict = json.loads(response.text)
-    emotions_data = response_dict['emotionPredictions'][0]['emotion']
-    
-    emotion_results = {'anger':emotions_data["anger"],
-    'disgust':emotions_data["disgust"],
-    'fear':emotions_data["fear"],
-    'joy':emotions_data["joy"],
-    'sadness':emotions_data["sadness"]}
+    if response.status_code == 200:
+        response_dict = json.loads(response.text)
+        emotions_data = response_dict['emotionPredictions'][0]['emotion']
+        emotion_results = {'anger':emotions_data["anger"],
+        'disgust':emotions_data["disgust"],
+        'fear':emotions_data["fear"],
+        'joy':emotions_data["joy"],
+        'sadness':emotions_data["sadness"]}
+        dominant_emotion = max(emotions_data, key=emotions_data.get)
+    elif response.status_code == 400:
+        emotion_results = {'anger':'None',
+        'disgust':'None',
+        'fear':'None',
+        'joy':'None',
+        'sadness':'None'}
+        dominant_emotion = 'None'
 
-    dominant_emotion = max(emotions_data, key=emotions_data.get)
-     
     return {'anger':emotion_results["anger"],
     'disgust':emotion_results["disgust"],
     'fear':emotion_results["fear"],
--- server.py
+++ server.py
@@ -7,16 +7,18 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_emotion():  
     text_to_analyze = request.args.get('textToAnalyze')
-    # Pass the text to the sentiment_analyzer function and store the response
     response_dict = emotion_detector(text_to_analyze)
-    # Extract the emotion scores from the response
     anger_score = response_dict.get('anger')
     disgust_score = response_dict.get('disgust')
     fear_score = response_dict.get('fear')
     joy_score = response_dict.get('joy')
     sadness_score = response_dict.get('sadness')
     dominant_emotion = response_dict.get('dominant_emotion')
-    # return formatted data
+    
+    # return results
+    if dominant_emotion == 'None':
+        return f"Invalid text! Please try again!."
+    
     return f"For the given statement, the system response is 'anger': {anger_score},\
     'disgust': {disgust_score},\
     'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}.\
